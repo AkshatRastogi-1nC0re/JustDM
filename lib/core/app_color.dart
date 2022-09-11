74eb66880016e6901b96ba1cd0430aa4fe46afbf
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2022-09-11 08:24:57 +0530

    ..

--- lib/core/app_color.dart
+++ lib/core/app_color.dart
@@ -8,4 +8,7 @@ class AppColor {
 
   static const darkGrey = Color(0xFFA6A3A0);
   static const lightGrey = Color(0xFFE5E6E8);
+
+  static const darkBlue = Color(0xFF0A1F44);
+  static const lightBlue = Color(0xFF0A1F44);
 }
