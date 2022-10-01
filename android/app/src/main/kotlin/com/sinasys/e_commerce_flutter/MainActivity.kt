9fa9e0f9a51e0cb3ced628df6472bdc5787224e8
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2022-10-01 07:32:33 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/sinasys/e_commerce_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/sinasys/e_commerce_flutter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sinasys.e_commerce_flutter
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterActivity() {
 }
